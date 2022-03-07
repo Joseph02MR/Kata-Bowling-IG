9cd61ad8b1c6bc6be73d83fcedad68225cb7d9f8
Joseph02MR <josmael0202@hotmail.com>
2022-03-07 15:55:23 -0600

    actualización base y pruebas

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    @Test
+    public void TestGame() throws Exception{
+        BowlingGame bowlingGameTest = new BowlingGame();
+
+    }
+
+}

b4ef0c18691e10bc44e10892602642f10c735322
Joseph02MR <josmael0202@hotmail.com>
2022-03-07 16:03:37 -0600

    prueba que falla

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,4 +1,10 @@
 public class BowlingGame {
 
+    public void roll(int i) {
+    }
+
+    public int score(){
+        return -1;
+    }
 }
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -7,6 +7,12 @@ class BowlingGameTest {
     public void TestGame() throws Exception{
         BowlingGame bowlingGameTest = new BowlingGame();
 
+        /*Ciclo del juego*/
+        for(int i = 0; i < 20; i++){
+            bowlingGameTest.roll(0);
+        }
+
+        assertEquals(0,bowlingGameTest.score());
     }
 
 }

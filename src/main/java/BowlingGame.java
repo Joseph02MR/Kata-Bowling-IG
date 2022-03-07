db0bbfccfe063e51d599733a17faa112ba79f722
Joseph02MR <josmael0202@hotmail.com>
2022-03-07 17:55:31 -0600

    Integración con código de iván

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,10 +1,24 @@
 public class BowlingGame {
 
-    public void roll(int i) {
+    public int score =0;
+    public int [] rolls = new int[21];
+    public int rollactual =0;
+    public BowlingGame bowlingObj;
+
+    public void tiradas (int n, int pins){
+        for(int i=0 ; i < n ;i++){
+            bowlingObj.roll(pins);
+        }
     }
 
-    public int score(){
-        return -1;
+    public void roll(int pins) {
+        score += pins;
     }
+
+    public int score (){
+        return score;
+    }
+
+
 }
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,11 +1,13 @@
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
+    BowlingGame bowlingGameTest;
     @Test
     public void TestGutterGame() throws Exception{
-        BowlingGame bowlingGameTest = new BowlingGame();
+
 
         /*Ciclo del juego*/
         for(int i = 0; i < 20; i++){
@@ -22,8 +24,35 @@ class BowlingGameTest {
         /*Ciclo del juego*/
         for(int i = 0; i < 20; i++){
             bowlingGameTest.roll(0);
-            a
         }
+    }
+
+    /*Código Iván*/
+    @BeforeEach
+    void setUp() {
+        BowlingGame bowlingGameTest = new BowlingGame();
+    }
+
+    @Test
+    public void Canaleta () throws Exception {
+        bowlingGameTest.tiradas(20,0);
+        assertEquals(0,bowlingGameTest.score());
+    }
+
+    @Test
+    public void prueba1pino () throws Exception {
+        bowlingGameTest.tiradas(20, 1);
+        assertEquals(20,bowlingGameTest.score());
+    }
+
+    @Test
+    public void pruebaSpare()throws Exception {
+        bowlingGameTest.roll(5);
+        bowlingGameTest.roll(5);
+        bowlingGameTest.roll(3);
+        bowlingGameTest.tiradas(17,0);
+        assertEquals(16, bowlingGameTest.score);
+
     }
 
 }

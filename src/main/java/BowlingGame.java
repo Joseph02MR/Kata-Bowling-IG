48d9acc8829bf54dce78d2318660f9a1a44d0d09
Eyebrock <eyebrock2913@gmail.com>
2022-03-09 16:45:28 -0600

    clase iván todo jala ya bien

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,24 +1,44 @@
 public class BowlingGame {
 
-    public int score =0;
+
     public int [] rolls = new int[21];
     public int rollactual =0;
-    public BowlingGame bowlingObj;
 
     public void tiradas (int n, int pins){
         for(int i=0 ; i < n ;i++){
-            bowlingObj.roll(pins);
+            this.roll(pins);
         }
     }
 
     public void roll(int pins) {
-        score += pins;
+        rolls[rollactual++] =pins;
     }
 
     public int score (){
+        int score =0;
+        int index=0;
+        for(int i=0;i< 10; i++)
+        {
+            if (rolls[index] == 10) {
+                score+=10 + rolls[index +1] +rolls[index +2];
+                index ++;
+            }
+            else if(EsSpare(index)){ //spares
+                score += 10 + rolls[index + 2];
+                index +=2;
+            }
+            else
+            {
+                score += rolls[index] + rolls[index + 1];
+                index+=2;
+            }
+        }
         return score;
     }
 
+        public boolean EsSpare(int index){
+        return rolls[index] + rolls[index + 1]==10;
+        }
 
 }
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -4,17 +4,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BowlingGameTest {
-    BowlingGame bowlingGameTest;
+    BowlingGame BowlingGameTest;
+
     @Test
     public void TestGutterGame() throws Exception{
 
 
         /*Ciclo del juego*/
         for(int i = 0; i < 20; i++){
-            bowlingGameTest.roll(0);
+            BowlingGameTest.roll(0);
         }
 
-        assertEquals(0,bowlingGameTest.score());
+        assertEquals(0,BowlingGameTest.score());
     }
 
     @Test
@@ -23,36 +24,59 @@ class BowlingGameTest {
 
         /*Ciclo del juego*/
         for(int i = 0; i < 20; i++){
-            bowlingGameTest.roll(0);
+            BowlingGameTest.roll(0);
         }
     }
 
     /*Código Iván*/
     @BeforeEach
     void setUp() {
-        BowlingGame bowlingGameTest = new BowlingGame();
+        BowlingGameTest = new BowlingGame();
     }
 
     @Test
     public void Canaleta () throws Exception {
-        bowlingGameTest.tiradas(20,0);
-        assertEquals(0,bowlingGameTest.score());
+        BowlingGameTest.tiradas(20,0);
+        assertEquals(0,BowlingGameTest.score());
     }
 
     @Test
     public void prueba1pino () throws Exception {
-        bowlingGameTest.tiradas(20, 1);
-        assertEquals(20,bowlingGameTest.score());
+        BowlingGameTest.tiradas(20, 1);
+        assertEquals(20,BowlingGameTest.score());
     }
 
     @Test
     public void pruebaSpare()throws Exception {
-        bowlingGameTest.roll(5);
-        bowlingGameTest.roll(5);
-        bowlingGameTest.roll(3);
-        bowlingGameTest.tiradas(17,0);
-        assertEquals(16, bowlingGameTest.score);
+        rollspare();
+        BowlingGameTest.roll(3);
+        BowlingGameTest.tiradas(17,0);
+        assertEquals(16, BowlingGameTest.score());
 
+    }
+
+    @Test
+    public void PruebaChuza(){
+        chuza();//CHUZAAAA
+        BowlingGameTest.roll(3);
+        BowlingGameTest.roll(4);
+        BowlingGameTest.tiradas(16,0);
+        assertEquals(24, BowlingGameTest.score());
+    }
+
+    @Test
+    public void JUEGOMACIZO (){
+        BowlingGameTest.tiradas(12,10);
+        assertEquals(300, BowlingGameTest.score());
+    }
+
+    public void rollspare(){
+        BowlingGameTest.roll(5);
+        BowlingGameTest.roll(5);
+    }
+
+    public void chuza(){
+        BowlingGameTest.roll(10);
     }
 
 }

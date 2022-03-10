41c25fa5c803d367eba8eee1c1e268466c87eb8e
Joseph02MR <josmael0202@hotmail.com>
2022-03-09 18:29:34 -0600

    prueba 4: juego con chuza prueba 5: juego perfecto práctica finalizada

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,7 @@
 public class BowlingGame {
 
-    public int score =0;
+    public int[] tiradas = new int[21];
+    public int roll_actual = 0;
 
 
     public void tiradas (int n, int pins){
@@ -10,12 +11,44 @@ public class BowlingGame {
     }
 
     public void roll(int pins) {
-        score += pins;
+        tiradas[roll_actual++] = pins;
     }
 
     public int score (){
+        int score = 0;
+        int index = 0;
+
+        for(int juego = 0; juego < 10; juego++){
+            //chuza
+            if(tiradas[index] == 10){
+                score += 10 + tiradas[index+1] + tiradas[index+2];
+                index++;
+            }
+            else if(esJuegoSpare(index)){
+                score += 10 + tiradas[index+2];
+                index+=2;
+            }
+            else{
+                score += tiradas[index] + tiradas[index+1];
+                index+=2;
+            }
+        }
         return score;
     }
+
+    private boolean esJuegoSpare(int index) {
+        return tiradas[index] + tiradas[index +1]==10;
+    }
+
+    public void jugadaChuza(){
+        roll(10);
+    }
+
+    public void jugadaSpare(){
+        roll(5);
+        roll(5);
+    }
+
 
 }
 
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -12,23 +12,39 @@ class BowlingGameTest {
     }
 
     @Test
-    public void Canaleta () throws Exception {
+    public void Canaleta () {
         bowlingGameTest.tiradas(20,0);
         assertEquals(0,bowlingGameTest.score());
     }
 
     @Test
-    public void prueba1pino () throws Exception {
+    public void prueba1pino () {
         bowlingGameTest.tiradas(20, 1);
         assertEquals(20,bowlingGameTest.score());
     }
 
     @Test
-    public void pruebaSpare()throws Exception {
-        bowlingGameTest.roll(5);
-        bowlingGameTest.roll(5);
+    public void pruebaSpare() {
+        bowlingGameTest.jugadaSpare();
         bowlingGameTest.roll(3);
         bowlingGameTest.tiradas(17, 0);
-        assertEquals(16, bowlingGameTest.score);
+        assertEquals(16, bowlingGameTest.score());
+    }
+
+    @Test
+    public void pruebaChuza() {
+        bowlingGameTest.roll(3);
+        bowlingGameTest.roll(5);
+        bowlingGameTest.jugadaChuza();
+        bowlingGameTest.roll(3);
+        bowlingGameTest.roll(5);
+        bowlingGameTest.tiradas(14,0);
+        assertEquals(34,bowlingGameTest.score());
+    }
+
+    @Test
+    public void pruebaJuegoPerfecto(){
+        bowlingGameTest.tiradas(12,10);
+        assertEquals(300,bowlingGameTest.score());
     }
 }
